93b4af2c034442f698949ed3ccd620973e5cca8b
Cleifson <cl.raujo1@gmail.com>
2024-07-09 00:27:15 -0300

    chore: add cast for date

--- app/Models/Despesa.php
+++ app/Models/Despesa.php
@@ -4,15 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Despesa extends Model
 {
     use HasFactory;
 
+    protected $table = 'despesas';
+
     protected $fillable = ['descricao', 'data', 'user_id', 'valor'];
+
+    protected $casts = [
+        'data' => 'date',
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($despesa) {
+            if (is_null($despesa->data)) {
+                $despesa->data = Carbon::now()->toDateString();
+            }
+        });
     }
 }
